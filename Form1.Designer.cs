b094045dffd859b9cd2287682cc847a6c5aa5b2c
Aaron Audet <aaafrench@gmail.com>
2021-02-07 17:41:26 -0500

    base

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Lab4B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkTags = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileStatus = new System.Windows.Forms.Label();
+            this.fileOutput = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(569, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFile,
+            this.exit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkTags});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // checkTags
+            // 
+            this.checkTags.Enabled = false;
+            this.checkTags.Name = "checkTags";
+            this.checkTags.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.checkTags.Size = new System.Drawing.Size(180, 22);
+            this.checkTags.Text = "Check Tags";
+            this.checkTags.Click += new System.EventHandler(this.checkTagsToolStripMenuItem_Click);
+            // 
+            // loadFile
+            // 
+            this.loadFile.Name = "loadFile";
+            this.loadFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.loadFile.Size = new System.Drawing.Size(180, 22);
+            this.loadFile.Text = "Load File";
+            this.loadFile.Click += new System.EventHandler(this.loadFileToolStripMenuItem_Click);
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exit.Size = new System.Drawing.Size(180, 22);
+            this.exit.Text = "Exit";
+            this.exit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // fileStatus
+            // 
+            this.fileStatus.AutoSize = true;
+            this.fileStatus.Dock = System.Windows.Forms.DockStyle.Top;
+            this.fileStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileStatus.Location = new System.Drawing.Point(0, 24);
+            this.fileStatus.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.fileStatus.Name = "fileStatus";
+            this.fileStatus.Padding = new System.Windows.Forms.Padding(0, 4, 0, 4);
+            this.fileStatus.Size = new System.Drawing.Size(116, 28);
+            this.fileStatus.TabIndex = 1;
+            this.fileStatus.Text = "No File Loaded";
+            // 
+            // fileOutput
+            // 
+            this.fileOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fileOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileOutput.FormattingEnabled = true;
+            this.fileOutput.ItemHeight = 18;
+            this.fileOutput.Location = new System.Drawing.Point(0, 52);
+            this.fileOutput.Name = "fileOutput";
+            this.fileOutput.Size = new System.Drawing.Size(569, 374);
+            this.fileOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 426);
+            this.Controls.Add(this.fileOutput);
+            this.Controls.Add(this.fileStatus);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Aaron\'s Simple HTML File Checker";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFile;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkTags;
+        private System.Windows.Forms.Label fileStatus;
+        private System.Windows.Forms.ListBox fileOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+/**
+ * Name: Aaron Audet
+ * Student Number: 000367464
+ * File Date: November 22, 2020
+ * Program purpose: The porpuse of the application is to check to see if an html file has balanced tags.
+ * Statement of Authorship: I, Aaron Audet, 000367464 certify that this material is my original work.  No other person's work has been used without due acknowledgement.
+ */
+
+namespace Lab4B
+{
+    public partial class Form1 : Form
+    {
+        Stack<string> fileContent = new Stack<string>();
+        Stack<string> fileName = new Stack<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Exits the application
+        /// </summary>
+        /// <param name="sender">Not Used</param>
+        /// <param name="e">Not Used</param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Loads the selected file into the application
+        /// </summary>
+        /// <param name="sender">Not Used</param>
+        /// <param name="e">Not Used</param>
+        private void loadFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Clear App
+            checkTags.Enabled = false;
+            fileStatus.Text = "No File Loaded";
+            fileOutput.Items.Clear();
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // File picker
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "HTML files(*.html)|*.html";
+
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get file name
+                    fileName.Push(Path.GetFileName(openFileDialog.FileName));
+
+                    //Input file content
+                    var fileStream = openFileDialog.OpenFile();
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        Regex tags = new Regex(@"<.+?>");
+                        var text = reader.ReadToEnd();
+                        MatchCollection matches = tags.Matches(text);
+                        int i = 0;
+                        Stack<string> reverseTags = new Stack<string>();
+
+                        // Remove attributes
+                        foreach(Match matche in matches)
+                        {
+                            if(matche.Value.Contains(" "))
+                            {
+                                string[] split = matche.Value.Split(' ');
+                                split[0] += ">";
+                                reverseTags.Push(split[0].ToLower());
+                            } else
+                            {
+                                reverseTags.Push(matche.Value.ToLower());
+                            }
+                        }
+
+                        // Fixing order
+                        foreach(var item in reverseTags)
+                        {
+                            fileContent.Push(item);
+                        }
+                    }
+
+                    // Updating App
+                    checkTags.Enabled = true;
+                    fileStatus.Text = $"Loaded: {fileName.First()}";
+                    
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks to see if all tags are balanced
+        /// </summary>
+        /// <param name="sender">Not Used</param>
+        /// <param name="e">Not Used</param>
+        private void checkTagsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int total = 0; // Total amount of items added to date
+            int tab = 0; // Indentation amount
+            List<string> list = new List<string>();
+            bool balance = true;
+
+
+            // Check Tags
+            foreach(var item in fileContent)
+            {
+                if(total == 0) // If First
+                {
+                    if(!item.Contains("/"))
+                    {
+                        fileOutput.Items.Add($"Found opening tag: {item}");
+                        list.Add(item);
+                        tab++;
+                    } else // Not balanced
+                    {
+                        fileOutput.Items.Add($"Found closing tag: {item}");
+                        balance = false;
+                        break;
+                    }
+                } else
+                {
+                    char t = Convert.ToChar(9);
+                    if (!item.Contains("/")) // If Opening
+                    {
+                        if(item == "<br>" || item == "<hr>" || item == "<img>")
+                        {
+                            fileOutput.Items.Add($"{new String(t, tab)}Found non-container tag: {item}");
+                        } else
+                        {
+                            fileOutput.Items.Add($"{new String(t, tab)}Found opening tag: {item}");
+                            list.Add(item);
+                            tab++;
+                        }
+                    } else if(item.Contains("/")) // If closing
+                    {
+                        if(list.Last() == item.Remove(1, 1))
+                        {
+                            tab--;
+                            fileOutput.Items.Add($"{new String(t, tab)}Found closing tag: {item}");
+                            list.RemoveAt(list.Count - 1);
+                        } else // Not balanced
+                        {
+                            balance = false;
+                            fileOutput.Items.Add($"{new String(t, tab)}Found closing tag: {item}");
+                            break;
+                        }
+                    }
+                }
+                total++;
+            }
+
+            // Check balance
+            if(balance)
+            {
+                fileStatus.Text = $"{fileName.First()} has balanced tags";
+            } else
+            {
+                fileStatus.Text = $"{fileName.First()} does not have balanced tags";
+            }
+        }
+    }
+}
